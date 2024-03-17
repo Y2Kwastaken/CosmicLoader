48cd8ee837c127b094752b48e0fec08d28dc3e18
Miles Holder <mwholder2005@gmail.com>
2024-03-17 12:09:16 -0500

    fix launch scripts

--- launchers/launcher.sh
+++ launchers/launcher.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Dfabric.skipMcProvider=true -Dfabric.development=true -classpath "cosmic-reach.jar:CosmicLoader-1.0.0-SNAPSHOT.jar:libs/*" net.fabricmc.loader.launch.knot.KnotClient
+java -Dfabric.skipMcProvider=true -Dfabric.development=true -classpath "cosmic-reach.jar:CosmicLoader-1.0.1-SNAPSHOT.jar:libs/*" net.fabricmc.loader.launch.knot.KnotClient
